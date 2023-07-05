d9db608e044458af8e3c86dbfb0ad5b8c2c7c3e7
MRX-S <94443320+MRX-S@users.noreply.github.com>
2023-07-05 03:04:39 -0300

    Add files via upload

---
+++ ColorBash.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#Autor MRX-S
+
+#Colours
+  greencolour="\e[0;32m\033[1m"
+  endcolour="\033[0m\e[0m"
+  redcolour="\e[0;31m\033[1m"
+  bluecolour="\e[0;34m\033[1m"
+  yellowcolour="\e[0;33m\033[1m"
+  purplecolour="\e[0;35m\033[1m"
+  turquoisecolour="\e[0;36m\033[1m"
+  graycolour="\e[0;37m\033[1m"
