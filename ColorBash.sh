c1db55e449b1fdce5d73aa2aa7c737e938863284
Y4CHI <94443320+Y4CHI@users.noreply.github.com>
2023-10-26 19:04:23 -0600

    UpdateBashColors.sh

--- ColorBash.sh
+++ ColorBash.sh
@@ -2,11 +2,11 @@
 #Autor: YACHI-LD
 
 #Colours
-  greencolour="\e[0;32m\033[1m"
-  endcolour="\033[0m\e[0m"
-  redcolour="\e[0;31m\033[1m"
-  bluecolour="\e[0;34m\033[1m"
-  yellowcolour="\e[0;33m\033[1m"
-  purplecolour="\e[0;35m\033[1m"
-  turquoisecolour="\e[0;36m\033[1m"
-  graycolour="\e[0;37m\033[1m"
+  green="\e[0;32m\033[1m"
+  end="\033[0m\e[0m"
+  red="\e[0;31m\033[1m"
+  blue="\e[0;34m\033[1m"
+  yellow="\e[0;33m\033[1m"
+  purple="\e[0;35m\033[1m"
+  turquel="\e[0;36m\033[1m"
+  gray="\e[0;37m\033[1m"
